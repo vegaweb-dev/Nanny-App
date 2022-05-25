6bf6c06bbb84748a7b45e2a5ccb47a4b35de2679
carlos <carlosjuniorrvega@hotmail.com>
2022-05-25 00:02:05 -0500

    feat:backend connected to frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Linda Love',
+      slug: 'nurse',
+      category: 'babby-sitter',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 12,
+      countInStock: 1,
+      brand: 'proveedores de servicios tercerizados sas',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        '20 years old baby sitter located in Barranquilla offers services as caregiver',
+    },
+    {
+      name: 'Linda Love',
+      slug: 'nurse',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'proveedores de servicios tercerizados sas',
+      rating: 4.0,
+      numReviews: 10,
+      description:
+        '20 years old baby sitter located in Barranquilla offers services as caregiver',
+    },
+    {
+      name: 'Linda Love',
+      slug: 'nurse',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'proveedores de servicios tercerizados sas',
+      rating: 4.5,
+      numReviews: 14,
+      description:
+        '20 years old baby sitter located in Barranquilla offers services as caregiver',
+    },
+    {
+      name: 'Linda Love',
+      slug: 'nurse',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'proveedores de servicios tercerizados sas',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        '20 years old baby sitter located in Barranquilla offers services as caregiver',
+    },
+  ],
+};
+export default data;
